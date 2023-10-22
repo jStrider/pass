d744e9d137b7e5a3341047164825bd3340c6f1cb
jStrider <ju.renaud974@gmail.com>
2023-10-22 04:54:31 +0200

    feat: improved helper and fixed some bugs

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module pass
 
 go 1.18
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- pass.go
+++ pass.go
@@ -1,21 +1,47 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
+	"github.com/joho/godotenv"
 )
 
-var password_store_dir = ""
+var password_store_dir string
 
-func initPassStore() (string, error) {
-	currentDir, err := os.Getwd()
-	os.Setenv("PASSWORD_STORE_DIR", currentDir)
-	fmt.Printf("passwordStore initialized to %s", currentDir)
-	return currentDir, err
+func initialize() {
+
+	err := godotenv.Load()
+    if err != nil {
+        fmt.Println("Error loading .env file")
+    }
+	password_store_dir = os.ExpandEnv(os.Getenv("PASSWORD_STORE_DIR"))
+	if _, err := os.Stat(password_store_dir); err != nil {
+		fmt.Println("passwordStore didn't exist, initializing")
+		os.MkdirAll(password_store_dir, 0777)
+	}
+}
+
+func initPassStoreDir(path string) (string, error) {
+	//if path is empty, set it to the current directory
+	var _path string
+	var err error
+	if path == "" {
+		_path,err = os.Getwd()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}else{
+		if _,err = os.Stat(path); err != nil {
+			log.Fatal(err)
+		}
+		_path = path
+	}
+	os.Setenv("PASSWORD_STORE_DIR", _path)
+	fmt.Printf("passwordStore initialized to %s", os.Getenv("PASSWORD_STORE_DIR"))
+	return _path, err
 }
 
 func listPassStore() {
@@ -34,28 +60,53 @@ func listPassStore() {
 	}
 }
 
-func helper() {
-	fmt.Println("Usage: pass <command>\n\nAvailable commands:\n\n" +
-		"    pass init\n" +
-		"    pass list\n" +
-		"    pass help <command>")
+func helper(option string) {
+	switch option {
+	case "init":
+		fmt.Println("Usage: pass init <path/to/directory>\n" +
+			"    Initializes the password store at the specified directory\n" +
+			"    If no directory is specified, the current directory is used\n")
+	case "list":
+		fmt.Println("Usage: pass list\n" +
+			"    Lists all the passwords in the password store\n")
+	case "insert":
+		fmt.Println("Usage: pass insert <path/to/secret>\n" +
+			"    Inserts a password at the specified path\n")
+		default:
+		fmt.Println("Usage: pass <command> \n" +
+			"Available commands:\n\n" +
+			"    pass init <path/to/directory>\n" +
+			"    pass list\n" +
+			"    pass insert <path/to/secret>\n" +
+			"    pass help <command>\n" +
+			" Environment Variables:\n" +
+			"    PASSWORD_STORE_DIR = directory where the password store is defined\n")
+		}
+
+}
+func insertPass(path string) {
+	nPath := password_store_dir + "/" + path
+	os.MkdirAll(nPath, 0777)
+	fmt.Println("inserting password at path: " + nPath)
 }
 func main() {
-	wordPtr := flag.String("word", "foo", "a string")
-	//do things based on os args
-	password_store_dir = os.Getenv("PASSWORD_STORE_DIR")
-	flag.Parse()
-	command := os.Args[1]
+	initialize()
+	var command string
+	if len(os.Args) > 1 {
+		command = os.Args[1]
+	}
 	switch command {
 	case "init":
-		initPassStore()
+		initPassStoreDir(os.Args[2])
 	case "list":
 		listPassStore()
 	case "help":
-		helper()
+		helper(os.Args[2])
+	case "insert":
+		insertPass(os.Args[2])
 	default:
-		helper()
+		helper("")
 	}
-
-	fmt.Println("word:", *wordPtr)
 }
+
+//TODO: create a config file
